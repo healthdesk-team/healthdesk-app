a13a7aa9ecad8830697f8e22ad7c162dfcca059f
Yann <yannick.jacquet@gmail.fr>
2018-08-05 02:37:13 +0200

    [*] create user into db after auth

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
+// Cloud Functions
+exports.createProfile = functions.auth.user().onCreate((user) => {
+  admin.initializeApp(functions.config().firebase)
+  return admin.firestore().collection('users').doc(user.uid).set({
+    email: user.email,
+    role: 'patient',
+    uid: user.uid,
+  })
 })
